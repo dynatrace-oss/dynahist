0f055afb187c337542a478b5609a12b0405e2dba
Otmar Ertl <otmar.ertl@gmail.com>
2020-08-20 08:14:54 +0200

    renamed package

--- src/test/java/com/dynatrace/dynahist/demo/PrintUtil.java
+++ src/test/java/com/dynatrace/dynahist/demo/PrintUtil.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.dynatrace.dynahist.examples;
+package com.dynatrace.dynahist.demo;
 
 import com.dynatrace.dynahist.Histogram;
 import com.dynatrace.dynahist.bin.BinIterator;
